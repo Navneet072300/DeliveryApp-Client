ab4322bf9e6730a47cc23da2f0de678626df4c6d
Navneet <navneetshahi345@gmail.com>
2024-08-02 13:33:19 +0530

    update category-filters.tsx

--- src/app/(routes)/menu/components/category-filters.tsx
+++ src/app/(routes)/menu/components/category-filters.tsx
@@ -3,7 +3,9 @@
 import Box from "@/components/Box";
 import { cn } from "@/lib/utils";
 import { Category } from "@/types-db";
+import { query } from "firebase/firestore";
 import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 interface CategoryFiltersProps {
   categories: Category[];
@@ -12,15 +14,32 @@ interface CategoryFiltersProps {
 const CategoryFilters = ({ categories }: CategoryFiltersProps) => {
   const searchParams = useSearchParams();
   const router = useRouter();
+
+  const handleCLick = (category: string) => {
+    const currentParams = Object.fromEntries(searchParams.entries());
+
+    if (currentParams.category === category) {
+      delete currentParams.category;
+    } else {
+      currentParams.category = category;
+    }
+
+    const href = qs.stringifyUrl({
+      url: "/menu",
+      query: currentParams,
+    });
+    router.push(href);
+  };
   return (
     <Box className=" flex-col gap-2 border-b pb-4 cursor-pointer">
       <h2 className=" text-xl font-semibold text-neutral-700">Category</h2>
       <Box className=" flex-col gap-2 mt-2">
         {categories?.map((category) => (
           <div
+            onClick={() => handleCLick(category.name)}
             key={category.id}
             className={cn(
-              "text-sm font-semibold text-neutral-50 flex items-center gap-2",
+              "text-sm font-semibold text-neutral-500 flex items-center gap-2",
               category.name === searchParams.get("category") && "text-hero"
             )}
           >
